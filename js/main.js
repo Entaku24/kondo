add8b213864985eda8bc2421f64d88524a7be641
Entaku24 <you@example.com>
2025-05-08 10:49:45 +0900

    画像なし・多セクション＋アニメーション対応

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // AOS 初期化
+  AOS.init({
+    duration: 800,
+    once: true
+  });
+
   // fullPage.js 初期化
   new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
     navigationPosition: 'right',
     scrollingSpeed: 700,
-    onLeave: () => {
+    afterLoad: () => {
       AOS.refresh();
     }
-  });
-
-  // AOS 初期化
-  AOS.init({
-    duration: 800,
-    once: true
   });
 });
